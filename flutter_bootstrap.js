e6ef7618c0a2e26ce4679550d9c9dad43ea8a1e5
YaelZamora <ajedrezkasparov@gmail.com>
2024-09-26 21:23:53 -0600

    Inventario prueba

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "47453962"
+    serviceWorkerVersion: "42061381"
   }
 });
