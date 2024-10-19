e9118b31004aa2382e1ec6ea8da580111ea5a2fe
YaelZamora <ajedrezkasparov@gmail.com>
2024-10-18 21:24:37 -0600

    Cambios en las vistas

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3463136479"
+    serviceWorkerVersion: "3211813687"
   }
 });
