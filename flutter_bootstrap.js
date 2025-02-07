33ee8c70fb1f6b951719129ce14acd0718692abf
YaelZamora <ajedrezkasparov@gmail.com>
2025-02-07 10:21:34 -0600

    Changes on UI

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2093396340"
+    serviceWorkerVersion: "635090007"
   }
 });
