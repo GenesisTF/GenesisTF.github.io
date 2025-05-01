6b07f7cad6385e82c43fd099e28bb0a11253ff1e
YaelZamora <ajedrezkasparov@gmail.com>
2025-05-01 13:06:23 -0600

    Code Review

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2139149581"
+    serviceWorkerVersion: "4070323530"
   }
 });
